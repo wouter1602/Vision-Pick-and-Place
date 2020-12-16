040a0838c315c9ce0e1770e9c0cd6f4f8ce127c3
Wouter <wouter@dropmeirl.com>
2020-12-16 12:24:34 +0100

    Get live video data

---
+++ main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import numpy as np
+import cv2 as cv
+import multiprocessing
+from dataclasses import dataclass
+
+
+class Settings:
+    DisplayOutput: bool
+    Verbose: bool
+    CaptureDevice: int
+
+    def __init__(self):
+        self.DisplayOutput = False
+        self.Verbose = False
+        self.CaptureDevice = 0
+
+
+class Video:
+
+    def __init__(self, captureDevice: int = 0):
+        self.__cap = cv.VideoCapture(captureDevice)
+
+    def video(self):
+        _, frame = self.__cap.read()
+        # frame = cv.flip(frame, 1)
+        return frame
+
+
+if __name__ == '__main__':
+    s = Settings()
+
+    # Set settings
+    s.DisplayOutput = True
+    s.Verbose = True
+    s.CaptureDevice = 0
+
+    if s.Verbose == True:
+        print("Test")
+
+    cap = Video(s.CaptureDevice)
+
+
+    while True:
+
+        frame = cap.video()
+
+        cv.imshow("frame", frame)
+
+        key = cv.waitKey(1)
+        if key == 27:  # Key 'S'
+            break
+    cv.waitKey(0)
+    cv.destroyAllWindows()
