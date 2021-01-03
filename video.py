6caa464d88283f8f1bdb35d68387bd3e33e2ebb9
Wouter <wouter@dropmeirl.com>
2021-01-03 18:07:13 +0100

    Added a documentation to the Video Class

--- video.py
+++ video.py
@@ -1,63 +1,137 @@
+# Video Class with all the object recognition functions
+
 from settings import Settings
+
+from typing import List
 import cv2 as cv
 import numpy as np
 
 
 class Video:
+    # Type hinting for all the public variables
+    frame: np.ndarray
+    grayscale: np.ndarray
+    blur: np.ndarray
+    canny: np.ndarray
+    thrash: np.ndarray
+    video_output: np.ndarray
+    dilate: np.ndarray
+    erode: np.ndarray
+    morphologyEx: np.ndarray
+
+    # Type hinting for all private variables
+    _cap: cv.VideoCapture
 
     def __init__(self, s: Settings):
+        """
+        Opens camera in the in Settings object classified location
+        :param s: Settings object used to store thresholds and camera used data
+        """
         if s.use_test_image:
+            # If defined in the Settings object the program wil use a static test image for testing purposes
             self._cap = cv.imread(s.test_image)
-            self.frame = self._cap
+            self.frame = cv.imread(s.test_image)
         else:
             self._cap = cv.VideoCapture(s.capture_device)
-            self._, self.frame = self._cap.read()
+            _, self.frame = self._cap.read()
 
-        self.grayscale = cv.cvtColor(self.frame, cv.COLOR_BGR2GRAY)
-        self.blur = cv.blur(self.frame, (1, 1))
-        self.canny = cv.Canny(self.blur, s.canny_threshold_A, s.canny_threshold_B)
-        self.lines = None
-        self.thrash = None
+        # This defines some of the transformations that you can get of the images
+        # self.grayscale = cv.cvtColor(self.frame, cv.COLOR_BGR2GRAY)
+        # self.blur = cv.blur(self.frame, (1, 1))
+        # self.canny = cv.Canny(self.blur, s.canny_threshold_A, s.canny_threshold_B)
+        # self.lines = None
+        # self.thrash = None
+        # self.video_output = self.frame.copy()
+        # This defines all the transformations that you can get of the images
+        self.grayscale = self.frame.copy()
+        self.blur = self.frame.copy()
+        self.canny = self.frame.copy()
+        self.thrash = self.frame.copy()
         self.video_output = self.frame.copy()
+        self.dilate = self.frame.copy()
+        self.erode = self.frame.copy()
+        self.morphologyEx = self.frame.copy()
 
-    def _video(self, s: Settings):
-        if s.use_test_image:
-            self.frame = self._cap
-        else:
-            self._, self.frame = self._cap.read()
+    def _get_video(self, s: Settings) -> None:
+        """
+        Gets the most recent image of the camera or returns static image if defined in the Settings object.
+        :param s: Settings object used to store thresholds and camera used data
+        :type s: Settings
+        :return: None
+        """
+        if not s.use_test_image:  # Function doesn't need to update the video frame if it's a static image
+            _, self.frame = self._cap.read()
 
-    def _grayscale(self):
+    def _convert_to_grayscale(self) -> None:
+        """
+        Converts the image to a grayscale
+        :return: None
+        """
         self.grayscale = cv.cvtColor(self.frame, cv.COLOR_BGR2GRAY)
 
-    def _edge_canny(self, s: Settings):
-        self._video(s)
-        self._grayscale()
+    def _edge_canny(self, s: Settings) -> None:
+        """
+        Uses the "canny_threshold_A" and "canny_threshold_B" to create an Canny view of the objects.
+        This function first grabs the most recent camera image an converts it to grayscale.
+        :param s: Settings object used to store thresholds and camera used data
+        :type s: Settings
+        :return: None
+        """
+        self._get_video(s)
+        self._convert_to_grayscale()
 
         self.canny = cv.Canny(self.grayscale, s.canny_threshold_A, s.canny_threshold_B)
 
-    def _threshold(self, s):
-        self._video(s)
-        self._grayscale()
+    def _threshold(self, s: Settings) -> None:
+        """
+        Uses the "contour_threshold_A" and "contour_threshold_B" to create an threshold view of the objects.
+        Turns part of the image white of black based on those thresholds.
+        This function first grabs the most recent camera image an converts it to grayscale.
+        :param s: Settings object used to store thresholds and camera used data
+        :type s: Settings
+        :return: None
+        """
+        self._get_video(s)
+        self._convert_to_grayscale()
 
         ret, self.thrash = cv.threshold(self.grayscale, s.contour_threshold_A, s.contour_threshold_B,
                                         cv.CHAIN_APPROX_NONE)
 
-    def _edge_contour(self, s: Settings, frame):
+    def _edge_contour(self, frame: np.ndarray) -> List:
+        """
+        Detects contours found in the image
+        :param frame: current frame you want edge detection on
+        :type frame: np.ndarray
+        :return: List with all the found contours
+        """
+        # hierarchy is not used
         contours, hierarchy = cv.findContours(frame, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
         return contours
 
-    def _draw_contour(self, s: Settings, contours):
+    def _draw_contour(self, s: Settings, contours: List) -> None:
+        """
+        Function draws the found lines on the current view window.
+        Uses the "contour_poly_threshold" to approximate the shapes of the objects.
+        :param s: Settings object used to store thresholds and camera used data
+        :type s: Settings
+        :param contours: List with all the contours in the found image
+        :type contours: List
+        :return: None
+        """
         self.video_output = self.frame.copy()
         for contour in contours:
             approx = cv.approxPolyDP(contour, s.contour_poly_threshold * cv.arcLength(contour, True), True)
-            # 0.01 can be adjusted for recognition
+            # contour_poly_threshold can be adjusted for recognition
+            # approximates the contours found into shapes
             cv.drawContours(self.video_output, [approx], 0, (0, 0, 0), 5)
             x = approx.ravel()[0]
             y = approx.ravel()[1] - 5
+            # if approx has 3 elements the object is a triangle
             if len(approx) == 3:
                 cv.putText(self.video_output, "Triangle", (x, y), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0))
                 if s.verbose:
                     print("Found triangle at:", x, ", ", y)
+            # if approx has 4 element the object is a square or rectangle
             elif len(approx) == 4:
                 x, y, w, h = cv.boundingRect(approx)
                 aspect_ratio = float(w) / h
@@ -69,47 +143,83 @@ class Video:
                     cv.putText(self.video_output, "Rectangle", (x, y), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0))
                     if s.verbose:
                         print("Found rectangle at:", x, ", ", y)
-            # elif len(approx) == 5:
-            #     cv.putText(self.video_output, "Pentagon", (x, y), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
-            # elif len(approx) == 10:
-            #     cv.putText(self.video_output, "Star", (x, y), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
-            # else:
-            #     cv.putText(self.video_output, "Circle", (x, y), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
 
     def _detect_lines(self, s: Settings):
+        """
+        Uses the HoughLineP function to detect lines.
+        :param s: Settings object used to store thresholds and camera used data
+        :type s: Settings
+        :return:
+        """
+        # TODO: place the variables in the Settings object so you can easily change them
+        # TODO: add type hinting to the return value
         return cv.HoughLinesP(self.canny, 1, np.pi / 180, 60, np.array([]), 50, 5)
 
-    def _draw_lines(self, s: Settings, lines):
+    def _draw_lines(self, lines) -> None:
+        """
+        Draws the lines found using the _detect_lines() function.
+        :param lines: List with lines found in the image TODO: update the type of list named here
+        :return: None
+        """
         self.video_output = self.frame.copy()
         for line in lines:
             for x1, y1, x2, y2 in line:
                 cv.line(self.video_output, (x1, y1), (x2, y2), (255, 0, 0), 3)
 
-    def _dilate_edges(self, iterations=1):
+    def _dilate_edges(self, iterations=1) -> None:
+        """
+        Enlarges the edges found using the _edge_canny() function. This is done to remove as many double lines as
+        possible
+        :param iterations: Non negative int for how many iterations the dilate function should be used for
+        :type iterations: int
+        :return: None
+        """
+        # TODO: change this function to use a variable instead of hard coding canny image
+        # TODO: find parameters that can be added to the Settings object
         kernel = np.ones((5, 5), np.uint8)
         self.dilate = cv.dilate(self.canny, kernel, iterations=iterations)
 
-    def _erode_edges(self):
+    def _erode_edges(self) -> None:
+        """
+        Erodes the edges of the detected lines.
+        This wil make the lines thinner and easier to detect using the detect lines functions.
+        :return: None
+        """
+        # TODO: change this function to use a variable instead of hard coding erode image
+        # TODO: add iterations
+        # TODO: find parameters that can be added to the Settings object
         kernel = np.ones((5, 5), np.uint8)
         self.erode = cv.erode(self.dilate, kernel)
         self.morphologyEx = cv.morphologyEx(self.erode, cv.MORPH_GRADIENT, kernel)
         self.erode = cv.erode(self.morphologyEx, kernel)
 
-    def detect_objects(self, s: Settings):
-        if s.edge_detection_type == 1:
+    def detect_objects(self, s: Settings) -> None:
+        """
+        Uses the in Settings specified method to detect the object
+        :param s: Settings object used to store thresholds and camera used data
+        :type s: Settings
+        :return: None
+        """
+        if s.edge_detection_type == 1:  # Threshold method
             self._threshold(s)
-            self._draw_contour(s, self._edge_contour(s, self.thrash))
-        elif s.edge_detection_type == 2:
+            self._draw_contour(s, self._edge_contour(self.thrash))
+        elif s.edge_detection_type == 2:  # Canny + filter method
             self._edge_canny(s)
             self._dilate_edges(1)
             self._erode_edges()
-            self._draw_contour(s, self._edge_contour(s, self.erode))
-        else:
+            self._draw_contour(s, self._edge_contour(self.erode))
+        else:  # Simple Canny method
             self._edge_canny(s)
             # self.draw_lines(s, self.detect_lines(s))
 
-    def update_live_feed(self, s: Settings):
-        self._video(s)
+    def update_live_feed(self, s: Settings) -> None:
+        """
+        Public method to update the video feed
+        :param s: Settings object used to store threshold and camera used data
+        :type s: Settings
+        :return: None
+        """
+        self._get_video(s)
 
     def __del__(self) -> int:
         # TODO: Create deconstruct
